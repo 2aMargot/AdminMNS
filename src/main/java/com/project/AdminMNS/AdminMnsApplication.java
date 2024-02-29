b9d3ae8919c2604d17e2959a4474c4e382e8316c
2aMargot <deza.margot@protonmail.com>
2024-02-29 14:53:43 +0100

    Init project

---
+++ src/main/java/com/project/AdminMNS/AdminMnsApplication.java
@@ -0,0 +1,13 @@
+package com.project.AdminMNS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminMnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminMnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/AdminMNS/AdminMnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AdminMNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminMnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
