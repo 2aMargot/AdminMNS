a05c3252c6f4660b20f8892c64f3402ba521c98f
2aMargot <deza.margot@protonmail.com>
2024-04-15 13:30:58 +0200

    Email and password validation

---
+++ src/main/java/com/project/adminmns/controller/StudentController.java
@@ -0,0 +1,97 @@
+package com.project.adminmns.controller;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.project.adminmns.dao.*;
+import com.project.adminmns.model.Student;
+import com.project.adminmns.security.AdminPermission;
+import com.project.adminmns.view.StudentView;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+@AllArgsConstructor
+public class StudentController {
+
+    StudentDao studentDao;
+    LatenessDao latenessDao;
+    AbsenceDao absenceDao;
+    StudentInscriptionFolderDao studentInscriptionFolderDao;
+
+    @GetMapping("/student/{id}")
+    @AdminPermission
+    @JsonView(StudentView.class)
+    public ResponseEntity<Student> get(@PathVariable int id) {
+
+        Optional<Student> studentOptional = studentDao.findById(id);
+
+        if (studentOptional.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(studentOptional.get(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/student/list")
+    @JsonView(StudentView.class)
+    @AdminPermission
+    public List<Student> list() {
+
+        return studentDao.findAll();
+    }
+
+    @PostMapping("/student")
+    @AdminPermission
+    @JsonView(StudentView.class)
+    public ResponseEntity<Student> add(@Valid @RequestBody Student newUser) {
+
+        if (newUser.getId() != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        studentDao.save(newUser);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/student/{id}")
+    @AdminPermission
+    @JsonView(StudentView.class)
+    public ResponseEntity<Student> modified(@Valid @RequestBody Student user, @PathVariable int id) {
+        user.setId(id);
+
+        Optional<Student> userOptional = studentDao.findById(user.getId());
+
+        if (userOptional.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        studentDao.save(user);
+        return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/student/{id}")
+    @AdminPermission
+    @JsonView(StudentView.class)
+    public ResponseEntity<Student> delete(@PathVariable int id) {
+
+        Optional<Student> userOptional = studentDao.findById(id);
+        Optional<Student> studentOptional = studentDao.findById(id);
+
+        if (userOptional.isPresent() && studentOptional.isPresent()) {
+
+            studentDao.deleteById(id);
+            studentDao.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+}
--- src/main/java/com/project/adminmns/model/Absence.java
+++ src/main/java/com/project/adminmns/model/Absence.java
@@ -1,5 +1,7 @@
 package com.project.adminmns.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.project.adminmns.view.StudentView;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -12,21 +14,27 @@ public class Absence {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "absence_id")
+    @JsonView(StudentView.class)
     protected Integer id;
 
     @Column(name = "absence_creation_date")
+    @JsonView(StudentView.class)
     protected LocalDate creationDate;
 
     @Column(name = "absence_start")
+    @JsonView(StudentView.class)
     protected LocalDate start;
 
     @Column(name = "absence_end")
+    @JsonView(StudentView.class)
     protected LocalDate end;
 
     @Column(name = "absence_justification")
+    @JsonView(StudentView.class)
     protected String justification;
 
     @Column(name = "absence_validity")
+    @JsonView(StudentView.class)
     protected Boolean validity;
 
     @ManyToOne
