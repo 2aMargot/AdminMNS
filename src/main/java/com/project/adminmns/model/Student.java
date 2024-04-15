628f1f6971b3e4a74eb143333e4dd2dc00950d60
2aMargot <deza.margot@protonmail.com>
2024-04-15 13:30:58 +0200

    Email and password validation

--- src/main/java/com/project/adminmns/model/Student.java
+++ src/main/java/com/project/adminmns/model/Student.java
@@ -1,5 +1,7 @@
 package com.project.adminmns.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.project.adminmns.view.StudentView;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -15,51 +17,63 @@ public class Student extends ModelUser{
 
     @Column(name = "student_birthdate")
     @NotBlank(message = "La date de naissance ne peut etre vide")
+    @JsonView(StudentView.class)
     protected LocalDate birthdate;
 
     @Column(name = "student_birthplace", length = 60)
     @NotBlank(message = "La ville de naissance ne peut etre vide")
     @Size(max = 60, message = "Le nom de la ville de naissance ne peux pas etre supérieur à maximum 60 caractères")
+    @JsonView(StudentView.class)
     protected String birthplace;
 
     @Column(name = "student_nationality", length = 100)
     @NotBlank(message = "La nationalité ne peut etre vide")
     @Size(max = 100, message = "La nationalité ne peux pas etre supérieur à maximum 100 caractères")
+    @JsonView(StudentView.class)
     protected String nationality;
 
     @Column(name = "student_postalcode", length = 20)
     @NotBlank(message = "Le code postal ne peut etre vide")
     @Size(max = 20, message = "Le code postal ne peux pas etre supérieur à maximum 20 caractères")
+    @JsonView(StudentView.class)
     protected String postalCode;
 
     @Column(name = "student_address")
     @NotBlank(message = "L'adresse ne peut etre vide")
+    @JsonView(StudentView.class)
     protected String address;
 
     @Column(name = "student_city", length = 100)
     @NotBlank(message = "La ville ne peut etre vide")
     @Size(max = 100, message = "La ville ne peux pas etre supérieur à maximum 100 caractères")
+    @JsonView(StudentView.class)
     protected String city;
 
     @Column(name = "student_phonenumber", length = 30)
     @Size(max = 30, message = "Le téléphone ne peux pas etre supérieur à maximum 30 caractères")
+    @JsonView(StudentView.class)
     protected String phoneNumber;
 
     @Column(name = "student_social_security_number", unique = true, length = 50)
     @Size(max = 50, message = "Le numéro de sécurité social ne peux pas etre supérieur à maximum 50 caractères")
+    @JsonView(StudentView.class)
     protected String socialSecurityNumber;
 
     @Column(name = "student_france_travail_number", unique = true, length = 30)
     @Size(max = 30, message = "Le numéro de france travail ne peux pas etre supérieur à maximum 30 caractères")
+    @JsonView(StudentView.class)
     protected String franceTravailNumber;
 
     @OneToMany(mappedBy = "studentAbsence", cascade = CascadeType.REMOVE)
+    @JsonView(StudentView.class)
     protected List<Absence> absenceList = new ArrayList<>();
 
     @OneToMany(mappedBy = "studentLateness", cascade = CascadeType.REMOVE)
+    @JsonView(StudentView.class)
     protected List<Lateness> latenessList = new ArrayList<>();
 
     @OneToMany(mappedBy = "student", cascade = CascadeType.REMOVE)
+    @JsonView(StudentView.class)
     protected List<StudentInscriptionFolder> studentInscriptionFolderList = new ArrayList<>();
 
 }
--- src/main/java/com/project/adminmns/model/StudentInscriptionFolder.java
+++ src/main/java/com/project/adminmns/model/StudentInscriptionFolder.java
@@ -1,5 +1,7 @@
 package com.project.adminmns.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.project.adminmns.view.StudentView;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -16,13 +18,16 @@ public class StudentInscriptionFolder {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(StudentView.class)
     protected Integer incriptionFolderId;
 
     @Column(name = "inscription_folder_creation_date")
     @NotBlank(message = "La date de creation du dossier d'inscription étudiant ne peut etre vide")
+    @JsonView(StudentView.class)
     protected LocalDate creationDate;
 
     @Column(name = "inscription_folder_deadline")
+    @JsonView(StudentView.class)
     protected LocalDate deadline;
 
     @ManyToOne(optional = false)
